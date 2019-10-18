22fded5889fb2a02dcb1abd31545394bd72096da
Sophie Crespo <sophiecrespo09@gmail.com>
2019-10-18 15:45:52 +0200

    create Maze class with pattern to dict function

--- maze.py
+++ maze.py
@@ -1,10 +1,10 @@
-maze_background = dict()
-with open("pattern.txt", "r", encoding = "utf8") as p:
-    for i, line in enumerate(p):
-        for j, letter in enumerate(line):
-            maze_background.update({(i,j): letter})
-
-for i in range(15):
-    for j in range(15):
-        print(maze_background.get((i,j)), end=" ")
-    print("\n")
+class Maze:
+    def __init__(self, pattern_path):
+        self.pattern_path = pattern_path
+    
+        background = dict()
+        with open(self.pattern_path, "r", encoding = "utf8") as p:
+            for i, line in enumerate(p):
+                for j, letter in enumerate(line):
+                    background.update({(i,j): letter})
+        self.background = background
