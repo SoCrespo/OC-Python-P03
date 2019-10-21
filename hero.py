d073d9a976bce8b71c737eeb040bed84d964f475
Sophie Crespo <sophiecrespo09@gmail.com>
2019-10-21 18:12:18 +0200

    create item and hero classes

--- hero.py
+++ hero.py
@@ -1,12 +1,8 @@
-class Item:
+class Hero():
     def __init__(self, name):
         self.name = name
-        # self.image = image
-        pass
-             
+        self.bag = []
+        #self.image = image
+        
     if __name__ == "__main__":
         print(self.name)
-            
-    
-
-    
