5211f956ca61364f3972cfab999fe4c97f83762c
Sophie Crespo <sophiecrespo09@gmail.com>
2019-10-29 15:23:40 +0100

    Add draft of tool management in game

--- game.py
+++ game.py
@@ -14,14 +14,15 @@ mac.pos, exit, corridor = get_positions()
 pos_for_tools = [(i,j) for (i,j) in corridor.keys() if (i, j) not in [mac.pos, exit]]
 tools_positions = random.choices(pos_for_tools, k=3)
 
-ether, needle, tube = (tool.Tool(img, pos) for img, pos in zip(
+tools = ether, needle, tube = (tool.Tool(img, pos) for img, pos in zip(
     (ether_img, needle_img, tube_img), tools_positions))
 
                  
 # Managing MacGyver movements
 new_coord = mac.pos
 while mac.pos != exit:
-    display_layout() 
+    display_layout()
+     
     for event in pygame.event.get(): 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
@@ -34,10 +35,19 @@ while mac.pos != exit:
                 new_coord = mac.left() 
             elif event.key == pygame.K_RIGHT:
                 new_coord = mac.right() 
+                
     if new_coord in corridor:
         background_dict[mac.pos]= "_"
         background_dict[new_coord] = "*"    
         mac.pos = new_coord
+    
+    for tool in tools:
+        if mac.pos == tool.pos:
+            mac.bag.append(tool)
+            tools.remove(tool) 
+    
+    #TODO : add condition to transform 3 tools into syringe                
+        
     
 print("Gagné !")
            
