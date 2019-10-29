7de0e102cf4539d4eee2950462731973e4a65ffe
Sophie Crespo <sophiecrespo09@gmail.com>
2019-10-29 09:32:51 +0100

    Fix indentation in maze for with open section

--- maze.py
+++ maze.py
@@ -6,13 +6,13 @@ background_dict = {}
 width = 0
 height = 0
 with open("pattern.txt", "r", encoding = "utf8") as p:
-            for i, line in enumerate(p):
-                if line.strip():
-                    height += 1
-                for j, char in enumerate(line.strip()):
-                    background_dict.update({(i,j): char})
-                    if i == 1 : 
-                        width += 1   
+    for i, line in enumerate(p):
+        if line.strip():
+            height += 1
+        for j, char in enumerate(line.strip()):
+            background_dict.update({(i,j): char})
+            if i == 1 : 
+                width += 1   
                 
                 
 # Set game window                        
