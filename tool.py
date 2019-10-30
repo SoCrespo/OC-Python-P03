6391ced4ec3f910855fb63bb807658ba88fdba10
Sophie Crespo <sophiecrespo09@gmail.com>
2019-10-30 09:29:15 +0100

    Replace attr img by letter in Tool class

--- tool.py
+++ tool.py
@@ -1,6 +1,6 @@
 class Tool:
-    def __init__(self, img, pos):
-        self.img = img
+    def __init__(self, letter, pos):
+        self.letter = letter
         self.pos = pos
         
 if __name__ == "__main__":
